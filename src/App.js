132211fe8c0e86f803714a7ca51253bfbdf4c4b4
Praphan Klairith <xbird007@gmail.com>
2019-06-09 00:07:09 +0700

    Update home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './bg.png';
 import './App.css';
+import 'normalize.css/normalize.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="htext">
+      หน้าแรก ประชาธิปไตย
       </div>
+       <div className="not-found">
+         หาไม่พบ หรือไม่มีสิทธิ์เข้าถึง
+       </div>
+       <div className="ext">
+        <a href="https://th.wikipedia.org/wiki/%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%8A%E0%B8%B2%E0%B8%98%E0%B8%B4%E0%B8%9B%E0%B9%84%E0%B8%95%E0%B8%A2" >เพิ่มเติม</a>
+       </div>
+
+    </div>
     );
   }
 }
